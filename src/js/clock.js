48faa0b5f70d31eef3229164d6f90b87339f5616
91aidenlee <91aiden.lee@gmail.com>
2020-12-29 00:02:57 +0900

    Save and load clock mode from local storage

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,5 +1,6 @@
 const clock = document.querySelector(".clock");
 const toggle = document.querySelector(".clock__toggle-btn");
+const today = document.querySelector(".date");
 let is24hourMode = true;
 
 // TODO : 날짜 표기
@@ -8,41 +9,47 @@ const getCurrentDate = () => {
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
-  //   console.log(`${year} ${month} ${day}`);
+  const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const currentMonth = monthList[month];
+  const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const dayOfWeek = week[date.getDay()];
+  today.innerText = `${dayOfWeek} ${currentMonth} ${day}, ${year}`;
 };
 
 // 24시간 표기 시계
-const getCurrentTime24 = () => {
+const getCurrentTime = () => {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
-  clock.innerText = `${hours > 9 ? hours : `0${hours}`}:${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`;
+  clock.innerText = is24hourMode
+    ? `${hours > 9 ? hours : `0${hours}`}:${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`
+    : `${hours > 11 ? `${hours - 12}` : `0${hours}`}:${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`;
 };
 
-// 12시간 표기 시계 (AM, PM)
-const getCurrentTime12 = () => {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-
-  // TODO : 자정일 때 0시로 표시하는 로직 필요
-  clock.innerText = `${hours > 11 ? `PM ${hours - 12}` : `AM 0${hours}`}:${minutes > 9 ? minutes : `0${minutes}`}:${
-    seconds > 9 ? seconds : `0${seconds}`
-  }`;
+const loadClockMode = () => {
+  const clockMode = localStorage.getItem("24hour");
+  if (clockMode == "false") {
+    is24hourMode = false;
+    toggle.value = "24 Hour";
+  } else {
+    is24hourMode = true;
+    toggle.value = "12 Hour";
+  }
 };
 
 const handleToggle = () => {
   is24hourMode = !is24hourMode;
-  !is24hourMode ? (toggle.value = "24 Hour") : (toggle.value = "AM / PM");
+  !is24hourMode ? (toggle.value = "24 Hour") : (toggle.value = "12 Hour");
+  localStorage.setItem("24hour", is24hourMode);
 };
 
 function init() {
+  loadClockMode();
   setInterval(() => {
     getCurrentDate();
-    !is24hourMode ? getCurrentTime12() : getCurrentTime24();
+    getCurrentTime();
   }, 1000);
 }
 
