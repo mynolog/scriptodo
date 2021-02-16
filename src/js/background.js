9a02cf3467cf43c969fbd9369686e61e843ea5c8
91aidenlee <91aiden.lee@gmail.com>
2021-02-16 19:29:11 +0900

    Reinit repo

--- src/js/background.js
+++ src/js/background.js
@@ -2,10 +2,6 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 4;
 
-function handleImgLoad() {
-  console.log("finished loading");
-}
-
 function paintImage(imgNumber) {
   const image = new Image();
   image.src = `src/img/${imgNumber}.jpg`;
